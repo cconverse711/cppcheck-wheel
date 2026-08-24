ee2e624eab4d6904729ccbdc4b5f4eda80b5d267
Cary Converse <cconverse711@gmail.com>
2026-08-24 00:30:05 -0400

    initial commit

---
+++ test/test_executable.py
@@ -0,0 +1,71 @@
+import os
+import pytest
+import sys
+import tempfile
+from pathlib import Path
+
+EXECUTABLES = (
+    "cppcheck",
+    "cppcheck-htmlreport",
+)
+
+@pytest.fixture(autouse=True)
+def ensure_cppcheck_from_wheel(monkeypatch):
+    """test the installed cppcheck package, not the local one"""
+    this_dir = Path(__file__).resolve().absolute().parent
+    for pd in (this_dir, this_dir / ".."):
+        try:
+            new_path = sys.path.remove(pd)
+            monkeypatch.setattr(sys, "path", new_path)
+        except ValueError:
+            pass
+    monkeypatch.delitem(sys.modules, "cppcheck", raising=False)
+
+@pytest.mark.parametrize("executable", EXECUTABLES)
+def test_executable_file(capsys, executable):
+    import cppcheck
+
+    cppcheck._get_executable.cache_clear()
+    exe = cppcheck.get_executable(executable)
+    assert os.path.exists(exe)
+    assert os.access(exe, os.X_OK)
+    assert capsys.readouterr().out == ""
+
+def test_verbose_output(capsys, monkeypatch):
+    import cppcheck
+    monkeypatch.setenv("CPPCHECK_WHEEL_VERBOSE", "1")
+    # need to clear cache to make sure the function is run again
+    cppcheck._get_executable.cache_clear()
+    cppcheck.get_executable("cppcheck")
+    assert capsys.readouterr().out
+
+def test_cppcheck():
+    import cppcheck
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        print(f"Created temporary directory at: {tmpdir}")
+        compilation_unit = Path(tmpdir) / "dummy.cpp"
+        with open(compilation_unit, "w") as ostr:
+            ostr.write("int main() { return 0;}\n")
+
+        # Verify that the addon and library files can be found.
+        xml_path = Path(tmpdir) / "report.xml"
+        assert (
+            cppcheck._run(
+                "cppcheck",
+                "--enable=all",
+                "--addon=naming",
+                "--library=posix",
+                "--xml",
+                f"--output-file={str(xml_path)}",
+                str(compilation_unit),
+            ) == 0
+        )
+
+        assert(
+            cppcheck._run_python(
+                "cppcheck-htmlreport",
+                f"--file={str(xml_path)}",
+                f"--report-dir={tmpdir}/html"
+            ) == 0
+        )
